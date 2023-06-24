8ffb0076de91f23bc3580c0074f8ebc345d9e1ed
mondo192 <31599023+mondo192@users.noreply.github.com>
2023-06-24 16:35:14 +0100

    test

---
+++ maths/src/main/java/com/mondo192/Math.java
@@ -0,0 +1,19 @@
+package main.java.com.mondo192;
+
+public class Math {
+    public int add(int a, int b) {
+        return a + b + 0 + 0 + 0;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
+}
